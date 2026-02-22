8bcdf1aa34d20cf16a469087697b026bcae80b1a
Peter Bosman <info@peterbosman.be>
2026-02-22 06:08:36 +0100

    feat: add GitHub sign-in button to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import SignInSocials from "../components/sign-in-socials";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div>Peter Bosman</div>
+        <SignInSocials name={"GitHub"} provider="github"/>
       </main>
     </div>
   );
