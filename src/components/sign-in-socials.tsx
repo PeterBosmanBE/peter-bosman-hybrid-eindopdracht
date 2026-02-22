a445386130146ecfc8d2df4e61c5c531528ac232
Peter Bosman <info@peterbosman.be>
2026-02-22 06:55:43 +0100

    chore: added Icons, changed button to shadcn

--- src/components/sign-in-socials.tsx
+++ src/components/sign-in-socials.tsx
@@ -1,23 +1,29 @@
 "use client";
-import { authClient } from "../lib/auth-client";
+import { Button } from "@/src/components/ui/button";
+import { authClient } from "@/src/lib/auth-client";
+import { Icon } from "./ui/icons";
 
 type SignInOptions = {
   name: string;
+  icon: "github";
   provider: "github";
 };
 
-export default function SignInSocials({ name, provider }: SignInOptions) {
+export default function SignInSocials({ name, icon, provider }: SignInOptions) {
   return (
     <>
-      <button
+      <Button
+        variant="outline"
+        className="h-12 rounded-lg border-border bg-secondary text-foreground hover:bg-muted transition-colors gap-3 font-medium"
         onClick={() =>
           authClient.signIn.social({
             provider: provider,
           })
         }
       >
-        Login to {name}
-      </button>
+        <Icon name={icon} />
+        {name}
+      </Button>
     </>
   );
 }
