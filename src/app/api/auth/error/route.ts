53a6e92b7784183e67fb239455cb2b6247edb882
Peter Bosman <info@peterbosman.be>
2026-02-23 19:32:16 +0100

    feat: add custom API route for login cancellation

---
+++ src/app/api/auth/error/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const error = searchParams.get("error");
+  const errorDescription = searchParams.get("error_description");
+
+  return NextResponse.json(
+    { error, error_description: errorDescription },
+    { status: 400 }
+  );
+}
