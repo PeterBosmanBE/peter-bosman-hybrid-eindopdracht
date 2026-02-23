f38a7dfa6c15a1d45fc7bd632988306e4a262a74
Peter Bosman <info@peterbosman.be>
2026-02-23 19:38:11 +0100

    chore: created avatar-dropdown menu

---
+++ src/components/avatar-dropdown.tsx
@@ -0,0 +1,62 @@
+"use client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/src/components/ui/dropdown-menu";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/src/components/ui/avatar";
+import { authClient } from "../lib/auth-client";
+import { Skeleton } from "@/src/components/ui/skeleton";
+import SignIn from "./sign-in";
+
+export default function AvatarDropdown() {
+  const { data: session } = authClient.useSession();
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar>
+            <AvatarImage
+              src={session?.user.image || "/icon.webp"}
+              alt={session ? session?.user.name + "'s Icon" : "User's Icon"}
+            />
+            <AvatarFallback>
+              <Skeleton className="size-10 shrink-0 rounded-full" />
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {session ? (
+            <DropdownMenuGroup>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuItem onClick={() => console.log("Testing")}>
+                Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuSeparator />
+            </DropdownMenuGroup>
+          ) : (
+            <DropdownMenuItem>
+              <SignIn />
+            </DropdownMenuItem>
+          )}
+          {session ? (
+            <DropdownMenuItem onClick={() => authClient.signOut()}>
+              Sign Out
+            </DropdownMenuItem>
+          ) : (
+            ""
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
