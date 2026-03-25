465d85935eed0b1cb0870003164f190bfdc6cfb4
Peter Bosman <info@peterbosman.be>
2026-03-25 01:40:28 +0100

    feat: added profile page and link to profile page in avatar dropdown

--- src/components/avatar-dropdown.tsx
+++ src/components/avatar-dropdown.tsx
@@ -16,6 +16,7 @@ import {
 import { authClient } from "@/src/server/auth/auth-client";
 import { Skeleton } from "@/src/components/ui/skeleton";
 import SignIn from "./sign-in";
+import Link from "next/link";
 
 export default function AvatarDropdown() {
   const { data: session } = authClient.useSession();
@@ -37,8 +38,8 @@ export default function AvatarDropdown() {
           {session ? (
             <DropdownMenuGroup>
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuItem onClick={() => console.log("Testing")}>
-                Profile
+              <DropdownMenuItem asChild>
+                <Link href={`/profile/${session.user.id}`}>Profile</Link>
               </DropdownMenuItem>
               <DropdownMenuItem>Billing</DropdownMenuItem>
               <DropdownMenuSeparator />
